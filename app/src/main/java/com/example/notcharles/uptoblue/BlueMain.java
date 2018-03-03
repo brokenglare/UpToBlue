813641b4ac50a4b84b0b3e2b65813eec849b59e2
brokenglare <bshermanva@gmail.com>
2018-03-03 14:27:43 -0500

    Test commit.

---
+++ app/src/main/java/com/example/notcharles/uptoblue/BlueMain.java
@@ -0,0 +1,14 @@
+package com.example.notcharles.uptoblue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// Test Comment
+public class BlueMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue_main);
+    }
+}

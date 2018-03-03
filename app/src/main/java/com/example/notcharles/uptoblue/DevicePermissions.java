6a7ec73c5c60efed94fa81615b8ecfb48d67cdf4
brokenglare <bshermanva@gmail.com>
2018-03-03 14:37:19 -0500

    Added second activity.

---
+++ app/src/main/java/com/example/notcharles/uptoblue/DevicePermissions.java
@@ -0,0 +1,13 @@
+package com.example.notcharles.uptoblue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DevicePermissions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_permissions);
+    }
+}
